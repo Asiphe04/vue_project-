b93bf02a8d4b2f5be8409db4b28792378471e6e3
Asiphe04 <asiphendimlana40@gmail.com>
2023-07-11 09:11:23 +0200

    admin_tbl

--- src/components/Table-Comp.vue
+++ src/components/Table-Comp.vue
@@ -1,35 +1,121 @@
 <template>
-     <table :to="{name: 'product', params: {id: product.id}}">
-    <tr>
-      <th>ID</th>
-      <th>Image</th>
+     <table :to="{name: 'product', params: {id: product.id}}" id="productTable">
+   <thead>
+      <tr>
+      <th class="td-id">ID</th>
+      <th class="td-img">Image</th>
       <th>Item Name</th>
-      <th>Description</th>
       <th>Price</th>
-      <th>Category</th>
+      <th class="td-desc">Description</th>
+      <!-- <th>Category</th> -->
       <th>Actions</th>
     </tr>
+  </thead>
+  <tbody id="productList">
     <tr>
-      <td>1</td>
-      <td><img :src="product.img" :alt="product.itemName" class="itemImg w-25"></td>
+      <td class="td-id">{{ product.id }}</td>
+      <td class="td-img"><img :src="product.img" :alt="product.itemName" class="w-25"></td>
       <td>{{product.itemName}}</td>
-      <td>{{product.desc}}</td>
-      <td>{{product.price}}</td>
-      <td>{{product.category}}</td>
+      <td>R{{product.price}}</td>
+      <td class="td-desc">{{ product.desc }}</td>
+      <!-- <td>{{product.category}}</td> -->
       <td>
-        <button class="edit-button">Edit</button>
-        <button class="delete-button">Delete</button>
+        <button class="editButton">Edit</button>
+        <button class="deleteButton">Delete</button>
       </td>
     </tr>
+  </tbody>
   </table>
 </template>
 <style>
-/* .itemImg{
-    width: 20%;
-} */
+button {
+  width: 70px !important;
+}
+.deleteButton {
+  padding: 6px 12px;
+  background-color: #dc3545;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+.editButton {
+  padding: 6px 12px;
+  background-color: #238636;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.deleteButton:hover {
+  background-color: #c82333;
+}
+
+.deleteButton:focus {
+  outline: none;
+}
+
+.deleteButton:active {
+  background-color: #bd2130;
+}
+
+.deleteButton[data-index] {
+  font-size: 14px;
+  font-weight: bold;
+}
+/* Table styles */
+#productTable {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+#productTable th,
+#productTable td {
+  padding: 8px;
+  color: white !important;
+  border: 1px solid #ddd;
+}
+
+#productTable th {
+  background-color: #010409 !important;
+}
+/* Media queries for smaller screens */
+@media only screen and (max-width: 768px) {
+  input {
+    width: 100% !important;
+  }
+
+  #productTable th,
+  #productTable td {
+    font-size: 12px;
+  }
+  .td-desc {
+    display: none;
+  }
+}
+
+@media only screen and (max-width: 480px) {
+  #productTable th,
+  #productTable td {
+    font-size: 10px;
+  }
+  .td-desc {
+    display: none;
+  }
+  .td-quantity {
+    display: none;
+  }
+  .td-img {
+    display: none;
+  }
+  .td-id {
+    display: none;
+  }
+}
 </style>
 <script>
  export default{
         props: ["product"]
     }
 </script>
+
